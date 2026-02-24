6f5666bf2d8b260973416e2d94823689d2b362e7
JohanaLainez <johanalainez000@gmail.com>
2026-02-24 17:48:41 -0600

    add

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const mensaje = document.getElementById("mensaje-js");
+
+    if (mensaje) {
+        mensaje.innerText = "Elige un personaje y empieza con lo básico";
+    }
+});
