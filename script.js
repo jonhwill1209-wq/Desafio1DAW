b0c7c93cb0c743cd9c3ce0e325dbc4444a9a3865
Smaki090 <mariasamour090@gmail.com>
2026-02-24 21:04:36 -0600

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const mensaje = document.getElementById("mensaje-js");
-
-    if (mensaje) {
-        mensaje.innerText = "Elige un personaje y empieza con lo básico";
-    }
-});
